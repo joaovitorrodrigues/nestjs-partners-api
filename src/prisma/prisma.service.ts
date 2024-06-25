49701b9f4509bd4de2c317689a4813ae1c397cc0
João Vitor Rodrigues <joaovitorbuxexa@hotmail.com>
2024-06-25 18:00:41 +0000

    corrections

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -3,7 +3,13 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
-  spot: any;
+  // private _spot: any;
+  // public get spot(): any {
+  //   return this._spot;
+  // }
+  // public set spot(value: any) {
+  //   this._spot = value;
+  // }
   async onModuleInit() {
     await this.$connect();
   }
